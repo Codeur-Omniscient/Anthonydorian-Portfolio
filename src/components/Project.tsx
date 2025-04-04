64a19841f5f30c72bad15e80fc3a127cdf68432d
Codeur-Omniscient <anthonymedin258@outlook.fr>
2025-04-04 23:22:08 +0100

    refactor(project): Amélioration de la structure et de l'espacement des éléments dans le composant Project et ProjectCard

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,29 +3,20 @@ import ProjectCard from "./ProjectCard";
 
 const Project = () => {
   return (
-    <section id="project" className="py-20 grid gap-5 ">
+    <section id="project" className="grid gap-5 py-20">
       <div className="mb-8">
-        <h2 className="text-4xl md:text-5xl font-bold mb-2">
+        <h2 className="mb-2 text-4xl font-bold md:text-5xl">
           Featured <span className="text-green-300">Projects.</span>
         </h2>
-        <p className="text-muted-foreground text-xl mb-4">
+        <p className="mb-4 text-xl text-muted-foreground">
           Here are some of the projects I have worked on. Feel free to check
           them out and explore!
         </p>
       </div>
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:grid-rows-5 md:gap-10">
-        <div className="md:row-span-2 card-modern">
-          <ProjectCard project={Projects[0]} />
-        </div>
-        <div className="md:row-span-2 md:col-start-2 md:row-start-2 card-modern">
-          <ProjectCard project={Projects[1]} />
-        </div>
-        <div className="md:row-span-2 md:row-start-3 card-modern">
-          <ProjectCard project={Projects[2]} />
-        </div>
-        <div className="md:row-span-2 md:col-start-2 md:row-start-4 card-modern">
-          <ProjectCard project={Projects[3]} />
-        </div>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        {Projects.map((project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
       </div>
     </section>
   );
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,26 +7,26 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
-    <>
+    <div className="card-modern">
       <div className="flex items-center">
         <img
           src={project.image}
           alt="Project banner"
-          className="object-cover object-center h-60"
+          className="h-60 object-cover object-center"
         />
       </div>
       <div className="flex flex-col gap-3 px-4 pb-2 pt-1">
         <div className="flex items-center justify-between text-lg font-semibold">
           <h3>{project.title}</h3>
           <a href={project.link} target="_blank" rel="noopener noreferrer">
-            <ExternalLink className="h-5 w-5 text-inherit cursor-pointer" />
+            <ExternalLink className="h-5 w-5 cursor-pointer text-inherit" />
           </a>
         </div>
         <div className="flex flex-wrap gap-2">
           {project.technologies.map((tech, i) => (
             <div
               key={tech.name + i}
-              className="flex items-center gap-2 px-3 py-1 text-sm rounded-md font-semibold"
+              className="flex items-center gap-2 rounded-md px-3 py-1 text-sm font-semibold"
               style={{ backgroundColor: tech.bgColor, color: tech.textColor }}
             >
               <span>{tech.icon && <tech.icon className="size-4" />}</span>
@@ -34,10 +34,10 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
             </div>
           ))}
         </div>
-        <div className="h-[1px] bg-gray-600 " />
+        <div className="h-[1px] bg-gray-600" />
         <p className="text-gray-400">{project.description}</p>
       </div>
-    </>
+    </div>
   );
 };
 
