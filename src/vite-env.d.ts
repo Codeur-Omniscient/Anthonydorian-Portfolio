44ac3cefc3df77768ac864de503d35c46914f395
Codeur-Omniscient <anthonymedin258@outlook.fr>
2025-04-14 23:40:53 +0100

    feat(env): Ajouter des interfaces ImportMetaEnv et ImportMeta pour la configuration de Locize

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_LOCIZE_PROJECTID: string;
+  readonly VITE_LOCIZE_APIKEY: string;
+  readonly VITE_LOCIZE_VERSION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
